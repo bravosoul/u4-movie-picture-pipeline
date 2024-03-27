4cd3511ecef9456e4ad63103452727f098d4e93c
bravosoul <59223679+bravosoul@users.noreply.github.com>
2024-03-27 23:02:14 +0530

    Update resources.py

--- starter/backend/movies/resources.py
+++ starter/backend/movies/resources.py
@@ -3,10 +3,10 @@ from flask.views import MethodView
 
 # Dummy database to hold movie examples
 movies = {
-    "123": {"title": "Top Gun: Maverick", "description": 
-            "Fighter planes"},
-    "456": {"title": "Sonic the Hedgehog", "description": 
-            "Blueer"},
+    "123": {"title": "Top Gun: Maverick", "description":  
+            "Fighter planes"}, 
+    "456": {"title": "Sonic the Hedgehog", "description":  
+            "Blueer"}, 
     "789": {"title": "A Quiet Place", "description": "Scary monsters"},
 }
 
@@ -16,7 +16,7 @@ class Movies(MethodView):
         if movie_id is None:
             # Return a list of all movies
             return jsonify({"movies": [dict({"title": movie["title"]}, 
-              **{"id": i}) for i, movie in movies.items()]})
+               **{"id": i}) for i, movie in movies.items()]})
         else:
             # Return the details of a specific movie
             return jsonify({"movie": movies[str(movie_id)]})
